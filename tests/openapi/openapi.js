5b1e8b4f8570f1b0e463744eba5b4dc3620f072c
Joaquín Ormaechea <joaquinormaechea@gmail.com>
2020-04-25 14:31:07 -0300

    Added validation for every 3.x OpenAPI version
    Fixes #6

--- tests/openapi/openapi.js
+++ tests/openapi/openapi.js
@@ -47,6 +47,17 @@ describe('Openapi', () => {
 			assert.deepStrictEqual(openapi, expectedOpenapi);
 		});
 
+		it('Should pass for any 3.x version', () => {
+
+			const openapi = parser.parse({
+				openapi: '3.1.3'
+			});
+
+			const expectedOpenapi = new Openapi('3.1.3');
+
+			assert.deepStrictEqual(openapi, expectedOpenapi);
+		});
+
 	});
 
 });
